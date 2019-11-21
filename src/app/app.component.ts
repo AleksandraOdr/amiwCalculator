17c23c6500aca03f192b667a77ff3643b5fc1584
AleksandraOdr <aleksandra.odrobinska@gmail.com>
2019-11-21 20:37:30 +0100

    code formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -7,33 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'amiwCalculator';
+  title = 'Calculator';
 
-  public text: string = '';
-  public val :number;
+  public text = '';
+  public val = 0;
 
 
   onClick(k: KeyboardEvent) {
     console.log(k);
-    if(k.key !== '0' && k.key !== '1'){
+    if (k.key !== '0' && k.key !== '1') {
       k.preventDefault();
     }
   }
 
-  onKeyUp(){
+  onKeyUp($event: KeyboardEvent) {
     console.log(this.text);
   }
-   
-  onDigitClick(ev : MouseEvent){
+
+  onDigitClick(ev: MouseEvent) {
     this.text += (ev.target as HTMLElement).textContent;
-   }
+  }
 
 
-   textToVal(){
-    this.val = parseInt(this.text,2);
-   }
-
-
-	  
-  
+  textToVal() {
+    this.val = parseInt(this.text, 2);
+  }
 }
